9e6cff53d00491e887ae984fc86b621a8020d8c1
rouk5555 <153026365+rouk5555@users.noreply.github.com>
2024-09-11 20:55:55 -0400

    Added Chrome Controller file

---
+++ week1/Chrome Controller.ps1
@@ -0,0 +1,10 @@
+﻿$chromeRunning = Get-Process -Name chrome -ErrorAction SilentlyContinue
+
+if ($chromeRunning) {
+    Write-Host "Closing Chrome"
+    Stop-Process -Name chrome -Force
+}
+else {
+    Write-Host "Opening Chrome"
+    Start-Process "chrome.exe" "https://www.champlain.edu"
+}
